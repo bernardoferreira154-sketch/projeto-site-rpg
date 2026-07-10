b85471538b18271bb7c384fca719ed7354407478
bernardoferreira154-sketch <bernardo_ferreira154@estudante.sesisenai.org.br>
2026-07-10 13:30:05 -0300

    adicionando imagens e concluindo provisóriamente a 2° seção

---
+++ script.js
@@ -0,0 +1,14 @@
+const elements = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("active");
+        }
+    });
+});
+
+elements.forEach((element) => {
+    observer.observe(element);
+})
+
