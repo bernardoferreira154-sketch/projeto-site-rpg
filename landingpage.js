5a9118bafcc250bc16d2bd6e834b323b1a4579c4
bernardoferreira154-sketch <bernardo_ferreira154@estudante.sesisenai.org.br>
2026-07-11 15:48:47 -0300

    ..

--- landingpage.js
+++ landingpage.js
@@ -13,3 +13,10 @@ elements.forEach((element) => {
 })
 
 
+window.addEventListener("pagereveal", (event) => {
+    if (event.viewTransition) {
+        console.log("✅ Transição aconteceu");
+    } else {
+        console.log("❌ Sem transição dessa vez");
+    }
+});
